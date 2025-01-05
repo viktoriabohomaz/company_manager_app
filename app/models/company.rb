b36c0d3915d353ff9b786f60b8a93ff6d049d1a5
Viktoria <viktoriazlayaa@gmail.com>
2025-01-06 00:34:55 +0100

    mend

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,6 @@ class Company < ApplicationRecord
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses
 
-  validates :name, presence: true
-  validates :registration_number, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 256 }
+  validates :registration_number, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
