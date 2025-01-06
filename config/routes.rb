4da9931235f671efb00526a93122b21a7a369dd2
Viktoria <viktoriazlayaa@gmail.com>
2025-01-06 01:29:20 +0100

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :companies do
+      resources :companies, only: [:index, :create, :show] do
         collection do
           post 'bulk_import', to: 'companies#bulk_import'
         end
